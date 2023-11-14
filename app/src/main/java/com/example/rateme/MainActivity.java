28af98ce960c282fd4ed6915f4c176fde89d8063
Tutar <emircan.tutar@hs-weingarten.de>
2023-11-14 19:49:13 +0100

    Barcode wird erfolgreich gescannt und gescannte nummer wird angezeigt.

--- app/src/main/java/com/example/rateme/MainActivity.java
+++ app/src/main/java/com/example/rateme/MainActivity.java
@@ -12,26 +12,73 @@ import androidx.navigation.ui.NavigationUI;
 
 import com.example.rateme.databinding.ActivityMainBinding;
 
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
+
+import com.google.zxing.integration.android.IntentIntegrator;
+import com.google.zxing.integration.android.IntentResult;
+
 public class MainActivity extends AppCompatActivity {
 
-    private ActivityMainBinding binding;
+    private AppBarConfiguration appBarConfiguration;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
 
-        binding = ActivityMainBinding.inflate(getLayoutInflater());
-        setContentView(binding.getRoot());
-
-        BottomNavigationView navView = findViewById(R.id.nav_view);
-        // Passing each menu ID as a set of Ids because each
-        // menu should be considered as top level destinations.
-        AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
+        // Setup für die Navigation
+        appBarConfiguration = new AppBarConfiguration.Builder(
                 R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications)
                 .build();
+
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_activity_main);
         NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
-        NavigationUI.setupWithNavController(binding.navView, navController);
+
+        // Navigation View
+        Button scanButton = findViewById(R.id.scanButton);
+        scanButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Starte den Barcode-Scan
+                new IntentIntegrator(MainActivity.this).initiateScan();
+            }
+        });
     }
 
+    // Diese Methode wird aufgerufen, wenn der Barcode-Scan abgeschlossen ist
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        IntentResult result = IntentIntegrator.parseActivityResult(requestCode, resultCode, data);
+        if (result != null) {
+            if (result.getContents() != null) {
+                // Hier kannst du die gescannten Daten verwenden
+                String scannedData = result.getContents();
+                // Verarbeite die gescannten Daten hier weiter
+                // Zum Beispiel: zeige die gescannten Daten in einem Toast an
+                Toast.makeText(this, "Gescannter Barcode: " + scannedData, Toast.LENGTH_LONG).show();
+            } else {
+                // Handle den Fall, dass der Scan abgebrochen wurde
+            }
+        }
+    }
+
+    // Füge diese Methode hinzu, um die Up-Navigation zu unterstützen
+    @Override
+    public boolean onSupportNavigateUp() {
+        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_activity_main);
+        return NavigationUI.navigateUp(navController, appBarConfiguration) || super.onSupportNavigateUp();
+    }
 }
