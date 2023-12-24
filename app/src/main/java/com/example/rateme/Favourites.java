76583cb05afcfe95f2a23b099a6f703d474e4a4e
Tutar <emircan.tutar@hs-weingarten.de>
2023-12-24 18:40:08 +0100

    Favourites entfernt

--- app/src/main/java/com/example/rateme/Favourites.java
+++ app/src/main/java/com/example/rateme/Favourites.java
@@ -1,4 +1,4 @@
-package com.example.rateme.ui.favourites;
+package com.example.rateme;
 
 import android.os.Bundle;
 import android.view.LayoutInflater;
@@ -8,24 +8,32 @@ import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.ViewModel;
 import androidx.lifecycle.ViewModelProvider;
 
 import com.example.rateme.databinding.FragmentFavouritesBinding;
 
-public class FavouritesFragment extends Fragment {
+public class Favourites extends Fragment {
 
     private FragmentFavouritesBinding binding;
+    private final MutableLiveData<String> mText;
+
+    public Favourites() {
+        mText = new MutableLiveData<>();
+        mText.setValue("Favourites");
+    }
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
-        FavouritesViewModel favouritesViewModel =
-                new ViewModelProvider(this).get(FavouritesViewModel.class);
 
         binding = FragmentFavouritesBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
 
         final TextView textView = binding.textFavourites;
-        favouritesViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
+        mText.observe(getViewLifecycleOwner(), textView::setText);
+
         return root;
     }
 
